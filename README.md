e24c1eb86e2f7666752a198d8bc713deaeefd8f9
ossim <mr.david.xie@gmail.com>
2019-10-15 23:21:00 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# spotify-light-mode
+# Spotify Light Mode
+
 Browser extension to bring Light Mode to the Spotify webplayer
+I'm not affiliated with Spotify by any means!
---
+++ chrome-ext/script.js
@@ -0,0 +1,7 @@
+console.log("lets go bnitch");
+
+var body = document.getElementsByTagName("body")[0];
+
+body.classList.add("lightmode");
+
+console.log(body, body.classList);
